1709fac1d79f70e4f6ee7ffe7a8f7fe7db818fe1
SuperVixen <SuperVixen@xaker.ru>
2023-02-24 11:14:55 +0300

    4-ое Задание закончено

--- Task_004/Program.cs
+++ Task_004/Program.cs
@@ -108,4 +108,78 @@
 // Console.WriteLine($"Массив: [ {String.Join(", ", resultArray)} ]");
 
 
+// // =============================================================
+// // Первая задача домашнего задания
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
 
+System.Console.Clear();
+Console.Write("Введите число А (основание степени): ");
+decimal basis = Convert.ToDecimal(Console.ReadLine());
+Console.Write("Введите число B (показатель степени): ");
+int degree = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+//Натуральные числа - положительные, целые с 1 до упавшей на бок 8
+decimal Exponentiation(decimal A, int B) // A = basis, B = degree
+{
+    decimal result = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        result *= A;   // sum = sum + i;
+    }
+    return result;
+}
+
+Console.WriteLine($"{basis} в степени {degree} = {Exponentiation (basis, degree)}");
+// // FIN
+
+
+
+// // =============================================================
+// // Вторая задача домашнего задания
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumOfDigits(int B) // B = number
+{
+    int result = 0;
+    while (B > 0)
+    {
+        result += B % 10; //Последняя цифра в результат
+        B /= 10;   // Отсекаем от исходного числа последнюю цифру
+    }
+    return result;
+}
+
+System.Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр в числе {number} = {SumOfDigits(number)}");
+// // FIN
+
+// // =============================================================
+// // Третья задача домашнего задания
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] GetArray(int size)
+{
+    int[] array = new int[size]; // Получается массив на size(8) элементов
+    // Массив состоит из 8 нулей
+    // size = array.Length
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(100); // [0;99]
+       
+    }
+    return array;
+}
+
+int[] resultArray = GetArray(8);
+ 
+System.Console.Clear();
+Console.WriteLine($"Массив: [ {String.Join(", ", resultArray)} ]");
+// // FIN
